41c09338783bc6dfce9d0d782d70938abbddf9c6
pig <896111958@qq.com>
2018-07-23 17:33:40 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from 'react'
+import './App.css'
+import BaseMap from './BaseMap'
+import MarkerFilter from './MarkerFilter'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.mapOptions = {
+      key: 'b22653ba2c2dba764dfebc7ced57dcd9',
+      center: {
+        longitude: 114.30553899999995,
+        latitude: 30.592849
+      },
+      zoom: 10
+    };
+    this.state = {
+      markers: [{
+        pos: {
+          longitude: 114.36432190000005,
+          latitude: 30.5360485
+        },
+        name: '武汉大学',
+        id: '1'
+      }, {
+        pos: {
+          longitude: 114.3614063,
+          latitude: 30.5592465
+        },
+        name: '华中科技大学',
+        id: '2'
+
+      }, {
+        pos: {
+          longitude: 114.34248439999999,
+          latitude: 30.5986568
+        },
+        name: '武汉理工大学',
+        id: '3'
+
+      }, {
+        pos: {
+          longitude: 114.36452859999997,
+          latitude: 30.5179264
+        },
+        name: '华中师范大学',
+        id: '4'
+
+      }, {
+        pos: {
+          longitude: 114.33832159999997,
+          latitude: 30.5169273
+        },
+        name: '武汉科技大学',
+        id: '5'
+
+      }]
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <MarkerFilter markers={this.state.markers}/>
+        <div className='map'><BaseMap mapOptions={this.mapOptions} markers={this.state.markers}/></div>
       </div>
-    );
+    )
   }
 }
-
 export default App;
